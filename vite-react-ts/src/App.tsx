52c1fc7c4e6e6cce8ffbaf8f828283a8e605bacd
Sendrayaperumal Balathandayutham <sendray.sanu@gmail.com>
2025-06-07 18:40:44 +0530

    Build issue fix with folder rename

--- vite-react-ts/src/App.tsx
+++ vite-react-ts/src/App.tsx
@@ -1,8 +1,11 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <div>
@@ -14,8 +17,19 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
