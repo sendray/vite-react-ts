3d80247b383c54abc9b03afefacb23abccc652ba
Sendrayaperumal Balathandayutham <sendray.sanu@gmail.com>
2025-06-07 18:30:58 +0530

    Typescript support added

--- vite-react-ts/src/App.tsx
+++ vite-react-ts/src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -18,7 +15,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
